140d65c00da8e44443514c2ac514fbe15506b676
Sohan Kamble <kamblesohan42@gmail.com>
2025-06-29 15:59:39 +0530

    codes updated

---
+++ app/config.py
@@ -0,0 +1,7 @@
+# config.py
+CHROMA_HOST = "localhost"
+DATABASE_URL= "postgresql+asyncpg://postgres:helloWorld44@localhost:5432/rbac_docs"
+
+
+JWT_SECRET = 'J1KnK0X_SyEJcd6ZhJVoX1Nhrrrrwwewwasd7u-NgPcD7U1ZmEd4'
+ALGORITHM = "HS256"
